b69e55bda726fde05450bf0debffc0d0383a0199
Paikea Melcher <paikeamelcher@proofschool.org>
2018-06-22 18:29:38 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -5,4 +5,5 @@ class Cat
     @name = name
     @mood = "nervous"
   end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -12,4 +12,20 @@ class Owner
   def say_species
     return "I am a human."
   end
+  
+  def buy_fish(name)
+    @pets[:fishes].push(name)
+  end
+  
+  def buy_cat(name)
+    @pets[:cats].push(name)
+  end
+  
+  def buy_dog(name)
+    @pets[:dogs].push(name)
+  end
+  
+  def list_pets
+    return "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end
  end
